c0ccab9ade8fcb54362af462c1dfa0f0a51187c2
Jackson Doherty <jackson@gardenlabs.com>
2024-12-11 17:58:43 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`genie-backend listening on port ${port}`);
+});
